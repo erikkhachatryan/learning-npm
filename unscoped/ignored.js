22ce1c8dacea4b506ea27f7aec51a7ee2e41135c
Erik Khachatryan <erik.khachatryan1996@gmail.com>
2020-07-05 21:24:01 +0400

    <Introduction to packages and modules>, <Contributing packages to the registry> chapters finished

---
+++ unscoped/ignored.js
@@ -0,0 +1,3 @@
+function log() {
+	console.log("This file is ignored, though it is not mentioned in the .npmignore file");
+}
---
+++ unscoped/src/included.js
@@ -0,0 +1,3 @@
+function log() {
+	console.log("A simple project js file");
+}
